02a6d6c65298aa16e53d2861f7794fde15d31a5f
Leonardo Silva <leonardo.sssobral@gmail.com>
2021-09-09 15:50:29 -0300

    Adicionar arquivos de projeto.

---
+++ FirstWebApp/Migrations/202109082202479_Observation.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Observation : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Product", name: "varchar", newName: "Name");
+            AddColumn("dbo.Product", "Observation", c => c.String(maxLength: 8000, unicode: false));
+            AlterColumn("dbo.Product", "Name", c => c.String(maxLength: 8000, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Product", "Name", c => c.String());
+            DropColumn("dbo.Product", "Observation");
+            RenameColumn(table: "dbo.Product", name: "Name", newName: "varchar");
+        }
+    }
+}
